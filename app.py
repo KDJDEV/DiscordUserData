c3f5188d81e970e16c9e537dadcecaaf4573875e
KDJDEV <76806009+KDJDEV@users.noreply.github.com>
2021-03-17 00:02:13 -0700

    yo

---
+++ app.py
@@ -0,0 +1,19 @@
+import flask
+from flask import Flask
+from flask_restful import Api, Resource
+import requests
+from os import environ
+token = environ['token']
+
+app = flask.Flask(__name__)
+api = Api(app)
+
+class GetUserInfo(Resource):
+	def get(self, userid):
+		data = requests.get(f"https://discord.com/api/v8/users/{userid}/profile", headers={"authorization":token)
+		json = data.json()
+		return json
+
+api.add_resource(GetUserInfo, "<string:userid>")
+if __name__ == "__main__":
+	app.run()
